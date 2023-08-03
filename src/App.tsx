c4eb0f016d893fbae2ac8ad6d4497781d00be1ef
IvayloSlavchev <ivaylo.slavchev124@gmail.com>
2023-08-03 08:47:58 +0300

    Addded error page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import StartScreen from './components/StartScreen/StartScreen';
 import QuestionScreen from './components/QuestionScreen/QuestionScreen';
 import EndScreen from './components/EndScreen/EndScreen';
+import ErrorPage from './components/ErrorPage/ErrorPage';
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
                 <Route path='/' element={<StartScreen />} />
                 <Route path='/questions' element={<QuestionScreen />} />
                 <Route path='/finish' element={<EndScreen />} />
+                <Route path='*' element={<ErrorPage />} />
             </Routes>
         </div>
     )
--- src/components/ErrorPage/ErrorPage.tsx
+++ src/components/ErrorPage/ErrorPage.tsx
@@ -3,7 +3,7 @@ import '../Styles/ErrorPage.css';
 const ErrorPage = () => {
     return (
         <div className='error-message'>
-            <h1>Sorry, something went wrong!</h1>
+            <h1>Sorry, something went wrong or page does not exist!</h1>
             <h1 className='status-code'>404</h1>
         </div>
     )
